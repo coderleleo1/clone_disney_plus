874539fe3126631ee71cbcd79938bfc05fce40d2
Leonardo <leobortoli2005@gmail.com>
2025-04-16 15:43:24 -0300

    pequeno ajuste dos arquivos

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ const sass = require('gulp-sass')(require('sass'));
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'))
 }
 
 exports.default = styles;
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.scss', gulp.series(styles))
 }
